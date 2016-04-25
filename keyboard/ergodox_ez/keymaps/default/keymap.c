546a0c5831c64ac0057e8936d0c6a4c62ce57540
nabetama <mao.nabeta@gmail.com>
2016-04-25 15:33:23 +0900

    Update keymap.c.

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -21,15 +21,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| LGui |           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  | paste| copy | Left | Right|                                       |  Up  | Down |   [  |   ]  | L2   |
+ *   |  L1  | paste| copy | Left | LGui |                                       | RGui | Down |   [  |   ]  | L2   |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | LGui | LAlt |       | Alt  | Esc  |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 |Backsp|Delete|------|       |------| Enter  |Space |
- *                                 |ace   |      | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                      ,--------------.          ,-------------.
+ *                                      | LGui  | LAlt |          | Alt  | Esc  |
+ *                                ,-----|------ |------|          |------+--------+------.
+ *                                |     |       | Home |          | PgUp |        |      |
+ *                                | ESC | Space |------|          |------| Space  |Enter |
+ *                                |     |       | End  |          | PgDn |        |      |
+ *                                `--------------------'          `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
@@ -39,19 +39,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TAB,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C),   KC_LEFT,KC_RGHT,
+        TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C),     KC_LEFT,GUI_T(KC_KANA),
                                                KC_LGUI,       KC_LALT,
                                                               KC_HOME,
-                                               KC_BSPC,KC_DELT,KC_END,
+                                               KC_ESC,KC_SPC,KC_END,
         // right hand
              KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TO(SYMB,1),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          TO(WINDOWS,1),
+                                  GUI_T(KC_HENK),  KC_DOWN,KC_LBRC,KC_RBRC, TO(WINDOWS,1),
              KC_LALT,        KC_ESC,
              KC_PGUP,
-             KC_PGDN,KC_ENT, KC_SPC
+             KC_PGDN,KC_SPC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
