c49d4a4203ca808cb0214470c4ad3f88fa283896
nabetama <mao.nabeta@gmail.com>
2016-04-28 14:31:31 +0900

    Update keymap.
    Add app, Ralt.

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| LGui |           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   <, |   >. |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  | paste| copy | Left | LGui |                                       | RGui | Down |   [  |   ]  | L2   |
+ *   |  L1  | paste| copy | App  | LGui |                                       | RGui | RAlt |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                      ,--------------.          ,-------------.
  *                                      | LGui  | LAlt |          | Alt  | Esc  |
@@ -39,7 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TAB,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C),     KC_LEFT,KC_LGUI,
+
+        TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C), ALT_T(KC_APP),KC_LGUI,
+
                                                KC_LGUI,       KC_LALT,
                                                               KC_HOME,
                                                KC_SPC,KC_ESC,KC_END,
@@ -48,7 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TO(SYMB,1),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_RGUI,  KC_DOWN,KC_LBRC,KC_RBRC, TO(WINDOWS,1),
+
+                                  KC_RGUI,  KC_RALT, KC_LBRC, KC_RBRC, KC_FN1,
+
              KC_LALT,        KC_ESC,
              KC_PGUP,
              KC_PGDN,KC_SPC, KC_ENT
