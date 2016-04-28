cfe931ff3bd0b4f0363c8e61010b7c5df17e2641
nabetama <mao.nabeta@gmail.com>
2016-04-28 14:09:07 +0900

    Fixed keymaps are left ⌘ , right ⌘ .

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TAB,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C),     KC_LEFT,GUI_T(KC_KANA),
+        TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C),     KC_LEFT,KC_LGUI,
                                                KC_LGUI,       KC_LALT,
                                                               KC_HOME,
                                                KC_SPC,KC_ESC,KC_END,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TO(SYMB,1),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  GUI_T(KC_HENK),  KC_DOWN,KC_LBRC,KC_RBRC, TO(WINDOWS,1),
+                                  KC_RGUI,  KC_DOWN,KC_LBRC,KC_RBRC, TO(WINDOWS,1),
              KC_LALT,        KC_ESC,
              KC_PGUP,
              KC_PGDN,KC_SPC, KC_ENT
