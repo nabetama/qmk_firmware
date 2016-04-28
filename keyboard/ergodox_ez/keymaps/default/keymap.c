8ce070d84843f1816bf5622fd7c0a3acdb9dd3a4
nabetama <mao.nabeta@gmail.com>
2016-04-28 14:50:29 +0900

    Add RESET button !!!

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |  M   |           | L0   |      |   1  |   2  |   3  |   \  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Left | Right|                                       |  Up  | Down |   0  |   =  |      |
+ *   | RESET|      |      | Left | Right|                                       |  Up  | Down |   0  |   =  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      | Mute |
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TAB,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_P,
        KC_LCTL,        KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
        KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_M,
-       KC_TRNS,        KC_TRNS,KC_TRNS,KC_LEFT,KC_RGHT,
+       RESET,        KC_TRNS,KC_TRNS,KC_LEFT,KC_RGHT,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_SPC, KC_ENT, KC_TRNS,
